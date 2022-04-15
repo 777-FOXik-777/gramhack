0289dee2b7e6b6ca6508610e6c027138b9ca2fee
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2022-04-15 09:34:50 +0300

    Update gram.sh

--- gram.sh
+++ gram.sh
@@ -1,5 +1,4 @@
-clear
-pkg install openssh -y
+
 clear
 string4=$(openssl rand -hex 32 | cut -c 1-4)
 string8=$(openssl rand -hex 32  | cut -c 1-8)
@@ -19,7 +18,7 @@ ig_sig="4f8732eb9ba7d1c8e8897a75d6474d4eb3f5279137431b2aafb71fafe2abe178"
 banner() {
 echo ""
 
-echo -e "\e[1;91m                                        ██                            ██"
+echo -e "\e[1;91m                                         ██                            ██"
 echo -e "\e[1;91m  ▄███▄██   ██▄████   ▄█████▄  ████▄██▄  ██▄████▄   ▄█████▄   ▄█████▄  ██ ▄██▀"
 echo -e "\e[1;91m ██▀  ▀██   ██▀       ▀ ▄▄▄██  ██ ██ ██  ██▀   ██   ▀ ▄▄▄██  ██▀    ▀  ██▄██"
 echo -e "\e[1;91m ██    ██   ██       ▄██▀▀▀██  ██ ██ ██  ██    ██  ▄██▀▀▀██  ██        ██▀██▄"
